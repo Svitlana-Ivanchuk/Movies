4a1e943bc2056a15181a0025b4e91b641a78288e
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-09-01 16:51:16 +0300

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { GlobalStyle } from './GlobalStyle';
+import HomePage from 'pages/HomePage';
+import MoviesPage from 'pages/MoviesPage';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />}>
+          <Route path="/movies/:movieId" element={<div>MovieDetails</div>} />
+        </Route>
+
+        <Route path="/movies/:movieId/cast" element={<div>Cast</div>} />
+        <Route path="/movies/:movieId/reviews" element={<div>Reviews</div>} />
+      </Routes>
+      <GlobalStyle />
+    </>
   );
 };
